d83b3d74f314d53c557ea1e9ce873b5e5c7cf0f4
Adam <adam.haldas@gmail.com>
2021-12-21 22:47:49 +0100

    add cursor events

--- app.js
+++ app.js
@@ -1,16 +1,81 @@
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-bar');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  nav.classList.toggle('active');
-});
+let controller;
+let slideScene;
+let pageScene;
 
 function animateSlides() {
+  //Init Controller
   controller = new ScrollMagic.Controller();
-
-  //select items
 
   const sliders = document.querySelectorAll('.slide');
   const nav = document.querySelector('.nav-header');
+  //loop over each slide
+  sliders.forEach((slide, index, slides) => {
+    const revealImg = slide.querySelector('.reveal-img');
+    const img = slide.querySelector('img');
+    const revealText = slide.querySelector('.reveal-text');
+    //GSAP
+    // gsap.to(revealImg, 1, { x: '100%' });
+    // gsap.to(revealText, 0.5, { x: '100%' });
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: 'power2' },
+    });
+    slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1');
+    slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=.75');
+    slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=.75');
+    // Tworzenie sceny
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      // .addIndicators({
+      //   colorStart: 'white',
+      //   colorTrigger: 'white',
+      //   name: 'slide',
+      // })
+      .addTo(controller);
+    //Nowa animacja
+    const pageTl = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+    pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' });
+
+    //Nowa scena
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: '100%',
+      triggerHook: 0,
+    })
+      // .addIndicators({
+      //   colorStart: 'white',
+      //   colorTrigger: 'white',
+      //   name: 'page',
+      //   indent: 200,
+      // })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
+      .addTo(controller);
+  });
 }
+
+animateSlides();
+
+const cursor = document.querySelector('.cursor');
+//funkcja kursora
+function cursorMove(e) {
+  cursor.style.top = e.pageY + 'px';
+  cursor.style.left = e.pageX + 'px';
+}
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === 'logo' || item.classList.contains('burger')) {
+    cursor.classList.add('nav-active');
+  } else {
+    cursor.classList.remove('nav-active');
+  }
+}
+window.addEventListener('mousemove', cursorMove);
+window.addEventListener('mouseover', activeCursor);
