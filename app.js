00d038e5553aac36fa8433c30d113820aab59703
hakue <adam.haldas@gmail.com>
2022-01-02 19:21:01 +0100

    add nav animation

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 let controller;
 let slideScene;
 let pageScene;
+const burger = document.querySelector(".burger");
 
 function animateSlides() {
   //Init Controller
   controller = new ScrollMagic.Controller();
 
-  const sliders = document.querySelectorAll('.slide');
-  const nav = document.querySelector('.nav-header');
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
   //loop over each slide
   sliders.forEach((slide, index, slides) => {
-    const revealImg = slide.querySelector('.reveal-img');
-    const img = slide.querySelector('img');
-    const revealText = slide.querySelector('.reveal-text');
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
     //GSAP
     // gsap.to(revealImg, 1, { x: '100%' });
     // gsap.to(revealText, 0.5, { x: '100%' });
     const slideTl = gsap.timeline({
-      defaults: { duration: 1, ease: 'power2' },
+      defaults: { duration: 1, ease: "power2" },
     });
-    slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' });
-    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '-=1');
-    slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '-=.75');
-    slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '-=.75');
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=.75");
+    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=.75");
     // Tworzenie sceny
     slideScene = new ScrollMagic.Scene({
       triggerElement: slide,
@@ -38,15 +39,15 @@ function animateSlides() {
       .addTo(controller);
     //Nowa animacja
     const pageTl = gsap.timeline();
-    let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
-    pageTl.fromTo(nextSlide, { y: '0%' }, { y: '50%' });
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
     pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
-    pageTl.fromTo(nextSlide, { y: '50%' }, { y: '0%' });
+    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" });
 
     //Nowa scena
     pageScene = new ScrollMagic.Scene({
       triggerElement: slide,
-      duration: '100%',
+      duration: "100%",
       triggerHook: 0,
     })
       // .addIndicators({
@@ -63,25 +64,32 @@ function animateSlides() {
 
 animateSlides();
 
-const cursor = document.querySelector('.cursor');
+const cursor = document.querySelector(".cursor");
 //funkcja kursora
 function cursorMove(e) {
-  cursor.style.top = e.pageY + 'px';
-  cursor.style.left = e.pageX + 'px';
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
 }
 function activeCursor(e) {
   const item = e.target;
-  console.log(item);
-  if (item.id === 'logo' || item.classList.contains('burger')) {
-    cursor.classList.add('nav-active');
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    cursor.classList.add("nav-active");
   } else {
-    cursor.classList.remove('nav-active');
+    cursor.classList.remove("nav-active");
   }
-  if (item.classList.contains('explore')) {
-    cursor.classList.add('explore-active');
+  if (item.classList.contains("explore")) {
+    cursor.classList.add("explore-active");
   } else {
-    cursor.classList.remove('explore-active');
+    cursor.classList.remove("explore-active");
   }
 }
-window.addEventListener('mousemove', cursorMove);
-window.addEventListener('mouseover', activeCursor);
+
+const navToggle = () => {
+  document.querySelector(".burger-inner").classList.toggle("active");
+  gsap.to(".nav-bar", 1, { clipPath: "circle(3000px at 100% -10%)" });
+};
+
+//Event Listeneers
+burger.addEventListener("click", navToggle);
+window.addEventListener("mousemove", cursorMove);
+window.addEventListener("mouseover", activeCursor);
